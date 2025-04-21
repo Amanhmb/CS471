1c2ee97df557e178a4f100f3a14dd9c4c9dec6b0
Amanhmb <amani676y@gmail.com>
2025-04-21 08:49:15 +0300

    Lab 7  update

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
  path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
  path('aboutus/', views.aboutus, name="books.aboutus"),
  path('search/', views.search),
+  path('simple/query', views.simple_query),
+  path('complex/query', views.complex_query),
+  
  ]
